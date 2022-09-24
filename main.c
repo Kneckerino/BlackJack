a5e6c8b8104568b6c0aa0ae79ad47ada6eb67458
Kneckerino <mlrennekamp@gmail.com>
2022-09-24 22:24:18 +0200

    Game officially works now, though it leaves a bit to be desired. Mainly making the game repeatable and thus making the currency system an actual feature. All other cards might need to become invisible in order to balance the game

--- main.c
+++ main.c
@@ -8,20 +8,16 @@ int playerCardAmount = 0;
 int dealerCardAmount = 0;
 int playerHand[52];
 int dealerHand[52];
-int deck[4][13];
+int deck[4][13]; //Playing deck
+char cards[4][13][6][10]; //All cards
 
 void ResetDeck();
 void InitCards();
 void ShowCards();
+void GiveCardsToDealer();
 void DealCard(int person);
 int IsBlackJack(int hand[], int size);
-
-char cards[4][13][6][10] = {{{{'-','-','-','-','-','-','-','-','-','-'},
-                              {'|','A',' ',' ',' ',' ',' ',' ',' ','|'},
-                              {'|',' ',' ',' ',' ',' ',' ',' ',' ','|'},
-                              {'|',' ',' ',' ',' ',' ',' ',' ',' ','|'},
-                              {'|',' ',' ',' ',' ',' ',' ',' ','A','|'},
-                              {'-','-','-','-','-','-','-','-','-','-'},}}};
+int GreatestLegalValue(int hand[], int size);
 
 int main() {
     srand(time(NULL)); //Initialize Random
@@ -31,7 +27,6 @@ int main() {
     int turn = 1;
     int money = 500;
     int bet;
-    int blackJack = 0;
 
     printf("Welcome to BlackJack!\n");
     printf("your total is %d\nHow much do you wager?>", money);
@@ -39,37 +34,70 @@ int main() {
         scanf(" %d", &bet);
         if (bet > money) printf("You don't have that kind of money, try again>");
     } while (bet > money);
+    money -= bet;
 
     //Deal cards to player then dealer then player then dealer again
     DealCard(player);
     DealCard(dealer);
     DealCard(player);
-    if (IsBlackJack(playerHand, playerCardAmount) == 1) printf("BlackJack! You won!");
+    if (IsBlackJack(playerHand, playerCardAmount) == 1) {
+
+        ShowCards();
+        printf("BlackJack! You won!");
+        return 0;
+    }
     DealCard(dealer);
-    if (IsBlackJack(dealerHand, dealerCardAmount) == 1) printf("Dealer had BlackJack, you lost...");
+    if (IsBlackJack(dealerHand, dealerCardAmount) == 1) {
+
+        ShowCards();
+        printf("Dealer had BlackJack, you lost...");
+        return 0;
+    }
 
     ShowCards();
 
     char answer;
-    printf("\nDo you wish to receive another card? y/n >");
     do {
-        scanf(" %c", &answer);
-        if (answer != 'y' && answer != 'n') printf("Could not determine answer, please enter again >");
+        printf("\nDo you wish to receive another card? y/n >");
+        do {
+            scanf(" %c", &answer);
+            if (answer != 'y' && answer != 'n') printf("Could not determine answer, please enter again >");
 
-    } while (answer != 'y' && answer != 'n');
+        } while (answer != 'y' && answer != 'n');
 
-    if (answer == 'y') {
-        DealCard(player);
-        ShowCards();
+        if (answer == 'y') {
+            DealCard(player);
+            ShowCards();
+
+            if (GreatestLegalValue(playerHand, playerCardAmount) == 0) {
+                printf("You have more than 21 \n You lost...\n");
+                bet = 0;
+                return 0;
+            }
+        }
+    } while (answer != 'n');
+
+    GiveCardsToDealer();
+    ShowCards();
+
+    int dealerCardVal = GreatestLegalValue(dealerHand, dealerCardAmount);
+    int playerCardVal = GreatestLegalValue(playerHand, playerCardAmount);
+    if (dealerCardVal >= playerCardVal) {
+        printf("Dealer (%d) had more points than you (%d) and won", dealerCardVal, playerCardVal);
+        bet = 0;
     }
-    else ; //End players turn and start dealing for the house
+    else {
+        printf("You (%d) had more points than dealer (%d) and won", playerCardVal, dealerCardVal);
+        money += bet * 2;
+    }
 }
 
 int IsBlackJack(int hand[], int size) {
+
     for (int i = 0; i < size; i++) {
         if (hand[i] % 13 == 0) {
             for (int j = 0; j < size; j++) {
-                if (hand[i] % 13 >= 10) return 1;
+                if (hand[j] % 13 >= 10) return 1;
             }
             return 0;
         }
@@ -78,7 +106,7 @@ int IsBlackJack(int hand[], int size) {
 }
 
 void ShowCards() { //Show dealers cards and players cards all face-up
-    printf("Dealer Hand:\n");
+    printf("Dealer Hand (%d):\n", GreatestLegalValue(dealerHand, dealerCardAmount));
     for (int i = 0; i < 6; ++i) {
         for (int j = 0; j < dealerCardAmount; j++) {
 
@@ -93,7 +121,7 @@ void ShowCards() { //Show dealers cards and players cards all face-up
         printf("\n");
     }
 
-    printf("Your Hand:\n");
+    printf("Your Hand (%d):\n", GreatestLegalValue(playerHand, playerCardAmount));
     for (int i = 0; i < 6; ++i) {
         for (int j = 0; j < playerCardAmount; j++) {
 
@@ -224,4 +252,42 @@ void DealCard(int person) {
     else if (person == 2) playerHand[playerCardAmount++] = dealtCard;
 
     else exit(EXIT_FAILURE);
+}
+
+void GiveCardsToDealer() {
+
+    while (GreatestLegalValue(dealerHand, dealerCardAmount) < 17) {
+        DealCard(dealer);
+    }
+    if (GreatestLegalValue(dealerHand, dealerCardAmount) > 21) {
+        printf("Dealer had more than 21, You win!\n");
+    }
+}
+
+int GreatestLegalValue(int hand[], int size) {
+
+    int totalValue = 0;
+    int aces = 0;
+    for (int i = 0; i < size; ++i) { //Find total value of all cards combined
+
+        int card = hand[i] % 13;
+
+        if (card > 0 && card < 9) totalValue += card + 1; //If it's a number 2-9
+
+        else if (card >= 9) totalValue += 10; //If it is a 10 or a face card
+
+        else if (card == 0) {totalValue += 11; aces++;}; //If it's an ace
+    }
+
+    if (totalValue > 21) { //If above limit then convert aces to 1 instead of 11
+        if (aces) {
+            for (int i = 0; i < aces; ++i) {
+                if (totalValue > 21) totalValue -= 10;
+            }
+            if (totalValue > 21) return 0; //You still lost
+            else return totalValue;
+        }
+        else return 0; //Lost game
+    }
+    else return totalValue;
 }
