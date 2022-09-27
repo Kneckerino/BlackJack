7249e8b871fcc2826c27f3d0767aedefa486cac9
Mikkel <mikkel.tygesen@gmail.com>
2022-09-27 23:49:26 +0200

    + Added system that saves players balance

---
+++ cmake-build-debug/CMakeFiles/BlackJack.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BlackJack.exe"
+  "BlackJack.exe.manifest"
+  "BlackJack.pdb"
+  "CMakeFiles/BlackJack.dir/main.c.obj"
+  "CMakeFiles/BlackJack.dir/main.c.obj.d"
+  "libBlackJack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BlackJack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
